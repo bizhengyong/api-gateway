a7ee54dedd62479cb5832232361a46fef26e8c6d
bizhengyong <bizhengyong_xy@163.com>
2020-05-26 21:51:26 +0800

    9-7-完成权限校验（上）

---
+++ src/main/java/com/imooc/apigateway/util/CookieUtil.java
@@ -0,0 +1,43 @@
+package com.imooc.apigateway.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author: bizy
+ * @date: 2020/05/25 20:16
+ */
+public class CookieUtil {
+
+
+    /**
+    *@description: 设置cookie
+    *@param: 
+    *@return: 
+    *@author: bizy
+    *@date: 2020/5/25 20:18
+    */
+    public static void set(HttpServletResponse response,
+                           String name,
+                           String value,
+                           int maxAge) {
+        Cookie cookie = new Cookie(name,value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public static Cookie get(HttpServletRequest request,
+                             String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    return cookie;
+                }
+            }
+        }
+        return null;
+    }
+}
